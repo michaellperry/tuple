b9283d2a3479f84e7c1e2ad81f3be2f72ae7b621
michaellperry <mperry@adventuresinsoftware.com>
2009-03-28 19:29:46 -0500

    Updated simplified BSD license.

--- src/main/java/com/mallardsoft/tuple/Tuple.java
+++ src/main/java/com/mallardsoft/tuple/Tuple.java
@@ -27,8 +27,11 @@ package com.mallardsoft.tuple;
  * http://mallardsoft.com
  * All rights reserved.
  * 
- * Redistribution and use of this software in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Simplified BSD license
+ * http://www.opensource.org/licenses/bsd-license.php
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
  * 
  *     * Redistributions of source code must retain the above copyright notice, this list
  *       of conditions and the following disclaimer.
@@ -39,7 +42,7 @@ package com.mallardsoft.tuple;
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
  * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
